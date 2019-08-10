30d57150055b8732d6b48ce562785b90c8f5fd3f
Veronica E <[veronica.eulenberg@gmail.com]>
2019-08-09 23:09:25 -0600

    init index.py

---
+++ index.py
@@ -0,0 +1,12 @@
+cities = [
+	{'city':'Salt Lake City, UT, USA',
+	'longitude':'111.8910',
+	'time':'23:00'},
+	{'city':'Mountain View, CA, USA',
+	'longitude':'122.0839',
+	'time':'22:00'}
+]
+
+print(cities[1]['city'])
+print(cities[1]['longitude'])
+print(cities[1]['time'])
