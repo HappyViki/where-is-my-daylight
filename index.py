1670aed4e940a52bd0307a4d3d559d86e15a3b9f
Veronica E <[veronica.eulenberg@gmail.com]>
2019-08-14 21:54:57 -0600

    dots split into 3 colors and changed data file name and variables

--- index.py
+++ index.py
@@ -1,35 +1,36 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Load Data
-df = pd.read_csv('timezone_trend.csv')
-df.columns = ['ID', 'Long', 'Local Time']
+df = pd.read_csv('data.csv')
+df.columns = ['cities', 'longitude', 'utc_offset']
 
-def scatterplot(df, x_dim, y_dim):
-    
-    # Variables
-    x = df[x_dim]
-    y = df[y_dim]
-    c = []
-    
-    # Setup Plot
-    fig, ax = plt.subplots(figsize=(10, 5))
+# Variables
+x_long = df['longitude']
+y_offset = df['utc_offset']
+c = []
 
-    for i in x:
-        if i < 0:
-            c.append('r')
-        else:
-            c.append('g')
-    
-    # Set Plot
-    ax.scatter(x, y, color=c)
-    
-    # Set Labels
-    ax.set_title('Timezone Trend')
-    ax.set_xlabel('Longitude')
-    ax.set_ylabel('Local Time')
-    
-    # Display Plot
-    plt.show()
-    
-scatterplot(df, 'Long', 'Local Time')
+# Setup Plot
+fig, ax = plt.subplots(figsize=(10, 5))
+plt.grid()
+
+for long,offset in zip(np.absolute(x_long),np.absolute(y_offset)):
+    center = 15*offset
+    if (long < center-15) or (long > center+15):
+        c.append('r')
+    elif (long < center-5) or (long > center+5):
+        c.append('y')
+    else:
+        c.append('g')
+
+# Set Plot
+ax.scatter(x_long, y_offset, color=c)
+
+# Set Labels
+ax.set_title('Timezone Trend')
+ax.set_xlabel('Longitude')
+ax.set_ylabel('Offset From UTC')
+
+# Display Plot
+plt.show()
